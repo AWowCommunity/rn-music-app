4417276a202769eee6ebd21e7b6953c85c6113f7
xufei <514286291@qq.com>
2023-08-03 16:41:14 +0800

    🌈 style:
    尝试 布局

--- App.js
+++ App.js
@@ -1,27 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, WhiteSpace } from '@ant-design/react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import { Button } from '@ant-design/react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>aaa</Text>
-      <Text>bbb</Text>
-      <View>
-        <Button type='primary'>Start</Button>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.wrapper}>
+          <Text>标题</Text>
+          <Text>列表</Text>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.box}>
+          <Text>body</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Text>footer</Text>
       </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
-});
+  header: {
+    width: '100%',
+    height: '6%',
+    backgroundColor: '#f50',
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  wrapper: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  body: {
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  box: {
+    width: 200,
+    height: 200,
+    borderWidth: 4,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footer: {
+  },
+})
